623dcc74e300dbdd9acc292388ea1991184ddb01
rrnaldi <renaldi240402@gmail.com>
2026-07-07 10:27:35 +0700

    day 3

--- tests/hari3.spec.ts
+++ tests/hari3.spec.ts
@@ -8,22 +8,18 @@ test("Tes Login dengan data benar", async ({ page }) => {
   await page.locator("#password").fill("secret_sauce");
 
   await page.locator("#login-button").click();
+
+  await expect(page).toHaveURL("https://www.saucedemo.com/cart.html");
 });
 
-test("Login gagal dengan password yang salah", async ({ page }) => {
+test("Tes Login dengan password kosong", async ({ page }) => {
   await page.goto("https://www.saucedemo.com");
 
   await page.locator("#user-name").fill("standard_user");
-  await page.locator("#password").fill("password_salah");
+
   await page.locator("#login-button").click();
 
-  await page.waitForTimeout(10000);
-});
-
-test("Login dengan User dan Password Kosong", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com");
-
-  await page.locator("#user-name").fill("");
-  await page.locator("#password").fill("");
-  await page.locator("#login-button").click();
+  await expect(page.locator('[data-test="error"]')).toHaveText(
+    "Epic sadface: Password is required",
+  );
 });
